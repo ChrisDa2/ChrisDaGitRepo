663c7c87dc7ad2c1bb12d679c6b52cac779cbde0
laurapelli <laurapelli2001@gmail.com>
2023-06-07 20:47:17 +0200

    Revert "Merge branch 'main' of https://github.com/ChrisDa2/ChrisDaGitRepo"
    This reverts commit 6c9f419a640c6dd8079290c3396f920be49536b9, reversing
    changes made to 70e6ca71f0a4e2bcd10cd36a4c3e045b31b210a0.

--- PLSNO-THEFLOORISLAVA/Assets/Scripts/PlatformController.cs
+++ PLSNO-THEFLOORISLAVA/Assets/Scripts/PlatformController.cs
@@ -1,38 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Netcode;
 
-public class PlatformController : NetworkBehaviour
+public class PlatformController : MonoBehaviour
 {
     private float speed = 0.01f;
-    //public GameObject colliderChecker;
-    private void Start()
-    {
-        Debug.Log("Empiezo");
-        //colliderChecker.gameObject.transform.SetParent(null);
-        NetworkObject.Spawn();
-    }
     private void Update()
     {
-        MovePlatforms();
-    }
-
-    private void MovePlatforms()
-    {
-        MovePlatformsServerRpc();
-    }
-    [ServerRpc]
-    private void MovePlatformsServerRpc()
-    {
-        MovePlatformsClientRpc();
-    }
-    [ClientRpc]
-    private void MovePlatformsClientRpc()
-    {
-        if (NetworkManager.Singleton.ConnectedClients.Count == 1)
-        {
-            transform.position += new Vector3(0, -speed, 0);
-        }
+        transform.position +=new Vector3(0, -speed, 0);
     }
 }
