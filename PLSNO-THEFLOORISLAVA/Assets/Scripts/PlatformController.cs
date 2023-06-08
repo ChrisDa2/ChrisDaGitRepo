6dfb949b1d1359f5a87dcdf5d88762abda7a173b
Thector4 <hector20_07@hotmail.es>
2023-06-08 11:54:32 +0200

    Platform y escena

--- PLSNO-THEFLOORISLAVA/Assets/Scripts/PlatformController.cs
+++ PLSNO-THEFLOORISLAVA/Assets/Scripts/PlatformController.cs
@@ -1,12 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class PlatformController : MonoBehaviour
+public class PlatformController : NetworkBehaviour
 {
     private float speed = 0.01f;
-    private void Update()
+
+    private void Start()
     {
-        transform.position +=new Vector3(0, -speed, 0);
+        Debug.Log("Empiezo");
+        NetworkObject.Spawn();
+    }
+    private void FixedUpdate()
+    {
+        if (!IsOwner) return;
+        MovePlatforms();
+    }
+    private void MovePlatforms()
+    {
+        MovePlatformsServerRpc();
+    }
+    [ServerRpc]
+    private void MovePlatformsServerRpc()
+    {
+        if(!IsServer && !IsHost)
+        {
+            transform.position += new Vector3(0, -speed, 0);
+        }
+        PruebonServerRpc();
+    }
+    [ServerRpc]
+    private void PruebonServerRpc()
+    {
+        MovePlatformss();
+    }
+    private void MovePlatformss()
+    {
+        if (NetworkManager.Singleton.ConnectedClients.Count == 3)
+        {
+            transform.position += new Vector3(0, -speed, 0);
+        }
     }
 }
